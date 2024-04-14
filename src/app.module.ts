392cd62026659f96f605c631560c94706d76253b
Manuel Eduardo Palta Caro <sekaidev@hotmail.com>
2024-04-14 19:09:58 -0400

    update index dto

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common'
 import { InventorysModule } from './inventorys/inventorys.module'
+import { UserModule } from './user/user.module';
+import { ProductModule } from './product/product.module';
+import { CategoryModule } from './category/category.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [InventorysModule]
+  imports: [InventorysModule, UserModule, ProductModule, CategoryModule, AuthModule]
 })
 export class AppModule {}
--- src/inventorys/inventorys.controller.ts
+++ src/inventorys/inventorys.controller.ts
@@ -1,34 +1,33 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { InventorysService } from './inventorys.service';
-import { CreateInventoryDto } from './dto/create-inventory.dto';
-import { UpdateInventoryDto } from './dto/update-inventory.dto';
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common'
+import { InventorysService } from './inventorys.service'
+import { CreateInventoryDto, UpdateInventoryDto } from './dto'
 
 @Controller('inventorys')
 export class InventorysController {
-  constructor(private readonly inventorysService: InventorysService) {}
+  constructor (private readonly inventorysService: InventorysService) {}
 
   @Post()
-  create(@Body() createInventoryDto: CreateInventoryDto) {
-    return this.inventorysService.create(createInventoryDto);
+  create (@Body() createInventoryDto: CreateInventoryDto) {
+    return this.inventorysService.create(createInventoryDto)
   }
 
   @Get()
-  findAll() {
-    return this.inventorysService.findAll();
+  findAll () {
+    return this.inventorysService.findAll()
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.inventorysService.findOne(+id);
+  findOne (@Param('id') id: string) {
+    return this.inventorysService.findOne(+id)
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateInventoryDto: UpdateInventoryDto) {
-    return this.inventorysService.update(+id, updateInventoryDto);
+  update (@Param('id') id: string, @Body() updateInventoryDto: UpdateInventoryDto) {
+    return this.inventorysService.update(+id, updateInventoryDto)
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.inventorysService.remove(+id);
+  remove (@Param('id') id: string) {
+    return this.inventorysService.remove(+id)
   }
 }
