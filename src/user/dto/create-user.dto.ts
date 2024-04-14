31b3fe297c8ab5c21261c4ea8af1c7020986d5a9
Manuel Eduardo Palta Caro <sekaidev@hotmail.com>
2024-04-14 18:02:32 -0400

    Update dto

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,12 +1,15 @@
-import { IsEmail, IsString } from 'class-validator'
+import { IsEmail, IsString, MinLength } from 'class-validator'
 
 export class CreateUserDto {
   @IsString()
+  @MinLength(4)
     name: string
 
   @IsEmail()
+  @IsString()
     email: string
 
   @IsString()
+  @MinLength(6)
     password: string
 }
--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -1,14 +1,25 @@
 import { PartialType } from '@nestjs/mapped-types'
 import { CreateUserDto } from './create-user.dto'
-import { IsString } from 'class-validator'
+import { IsEmail, IsOptional, IsString, MinLength } from 'class-validator'
 
 export class UpdateUserDto extends PartialType(CreateUserDto) {
   @IsString()
+  @IsOptional()
+  @MinLength(4)
     name?: string
 
   @IsString()
+  @IsEmail()
+  @IsOptional()
     email?: string
 
   @IsString()
-    password?: string
+  @MinLength(6)
+  @IsOptional()
+    currentPassword?: string
+
+  @IsString()
+  @MinLength(6)
+  @IsOptional()
+    newPassowrd?: string
 }
