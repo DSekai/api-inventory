f30d768adc8abdc2016f51689279c20aeba883a8
Manuel Eduardo Palta Caro <sekaidev@hotmail.com>
2024-04-16 03:18:09 -0400

    Updated and Addes new props

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common'
 import { InventorysModule } from './inventorys/inventorys.module'
-import { UserModule } from './user/user.module';
-import { ProductModule } from './product/product.module';
-import { CategoryModule } from './category/category.module';
-import { AuthModule } from './auth/auth.module';
+import { ProductModule } from './product/product.module'
+import { CategoryModule } from './category/category.module'
+import { AuthModule } from './auth/auth.module'
 
 @Module({
-  imports: [InventorysModule, UserModule, ProductModule, CategoryModule, AuthModule]
+  imports: [InventorysModule, ProductModule, CategoryModule, AuthModule]
 })
 export class AppModule {}
